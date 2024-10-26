b12a326aa28677e64f51c6da1236c1a71ac37274
James Calder <154292961+jcxldxr816@users.noreply.github.com>
2024-10-26 16:09:32 -0500

    quick test to see if main is being updated

--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //testing comments on main-branch
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
