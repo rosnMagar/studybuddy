1aedd20499562977ae6b4226226e36c5f3b442f0
James Calder <154292961+jcxldxr816@users.noreply.github.com>
2024-10-26 16:00:25 -0500

    test commit

--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+//    comment test - james
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
