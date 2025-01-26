f0b47b4a5584f4fc6b55c46d564636a2bda06237
rosnMagar <roshan.thamagar@gmail.com>
2025-01-26 14:57:45 -0600

    Added Tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,48 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { UserCircle, Compass, Send, LocateIcon } from "lucide-react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs>
+      {/* Home Tab */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Compass color={color} size={size} />
+          ),
+        }}
+      />
+      {/* Profile Tab */}
+      <Tabs.Screen
+        name="messages"
+        options={{
+          tabBarLabel: "Messages",
+          tabBarIcon: ({ color, size }) => (
+            <Send color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="location"
+        options={{
+          tabBarLabel: "Find People",
+          tabBarIcon: ({ color, size }) => (
+            <LocateIcon color={color} size={size} />
+          ),
+        }}
+      />
+      {/* Profile Tab */}
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <UserCircle color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
